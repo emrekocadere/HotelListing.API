0dd1a76a7f836fcfbf97b69f3c4ef7c7a76be7f3
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-05-21 20:44:08 +0300

    Add Hotel Entity

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Raiting { get; set; }
+        public int CountryID { get; set; }
+        public Country Country { get; set; }
+    }
+}
