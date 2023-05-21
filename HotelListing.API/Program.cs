6d93f276cde0841888174128f8e5783e83c4323d
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-05-21 23:54:29 +0300

    Add Properties into Country Entity

--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString ");
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
