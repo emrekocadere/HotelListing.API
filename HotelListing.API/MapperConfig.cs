f8849a716b8f7309f29feabf248ab7299183cc14
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-05-26 22:34:00 +0300

    feat: Add MapperConfig class for mapping between Country and CountryDTO

---
+++ HotelListing.API/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HotelListing.API.Data;
+using HotelListing.API.DTO_s;
+
+namespace HotelListing.API
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Country,CountryDTO>().ReverseMap();   
+        }
+    }
+}
