a240eb5378403d3f55053036f8b382f8d525de71
emrekocadere <85678350+emrekocadere@users.noreply.github.com>
2023-05-28 14:57:20 +0300

    feat: Add HotelDTO class for representing hotel data

---
+++ HotelListing.API/DTO's/HotelDTO.cs
@@ -0,0 +1,14 @@
+﻿using HotelListing.API.Data;
+
+namespace HotelListing.API.DTO_s
+{
+    public class HotelDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Raiting { get; set; }
+        public int CountryID { get; set; }
+
+    }
+}
